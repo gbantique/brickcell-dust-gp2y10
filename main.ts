d7d64897bfa3a988e54211e759708e27c6864fb5
gbantique <gbantique@gmail.com>
2023-10-17 08:21:28 +0800

    moved main.ts to test.ts for extension purpose

--- main.ts
+++ main.ts
@@ -1,7 +1,1 @@
-Brickcell.initializeFineDustSensor(AnalogPin.P0, DigitalPin.P1)
-serial.setBaudRate(BaudRate.BaudRate115200)
-basic.forever(function () {
-    serial.writeNumber(Brickcell.readFineDust())
-    serial.writeString("" + ("\r\n"))
-    basic.pause(2000)
-})
+
--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+Brickcell.initializeFineDustSensor(AnalogPin.P0, DigitalPin.P1)
+serial.setBaudRate(BaudRate.BaudRate115200)
+basic.forever(function () {
+    serial.writeNumber(Brickcell.readFineDust())
+    serial.writeString("" + ("\r\n"))
+    basic.pause(2000)
+})
